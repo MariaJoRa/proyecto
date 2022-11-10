cd92359dc9602f4f85b69d0a02e12f743058a8d1
MariaJoRa <115585159+MariaJoRa@users.noreply.github.com>
2022-11-10 16:48:47 -0500

    Add files via upload

---
+++ FinalProyect.java
@@ -0,0 +1,424 @@
+package proyectoFinal;
+
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class FinalProyect {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String[] alemania= {"Johannes Gutenberg", "Wolfgang von Goethe", "Albert Einstein", "Dieter Fehrenbach",
+				"Walter Reiner", "Rudolf Fischer", "Matthias Schmidt", "Ludwig Muller", "Franz Lehmann",
+				"Heinrich Wagner", "Immanuel Kant", "Sigmund Schubert", "Otto Bauer", "Helmut Kohl", "Bertram Bier",
+				"Elies Hoffmann","Maximiliam Weber"};
+		String[] posicionesAlemania= {"Arquero", "Defensa", "Defensa","Defensa","Defensa","Delantero","Delantero",
+				"Delantero", "Medio campo", "Medio campo","Medio campo","Delantero", "Defensa", "Medio campo",
+				"Medio campo", "Delantero","Defensa"};
+		ArrayList<String> nuevoJugadorAlemania= new ArrayList<String>();
+		ArrayList<String> posicionNuevoAlemania= new ArrayList<String>();
+		String[] estadoAlemania= {"En campo","En campo", "En campo","En campo","En campo","En campo","En campo","En campo",
+				"En campo","En campo","En campo","En banca","En banca","En banca","En banca","En banca","En banca",};
+		
+		
+		String[] argentina= {"Mario Malario", "Lionel Messi", "Pedro Gónzales", "Thiago Díaz", "Santino Martinez",
+				"Tomás Moreno","Matías Flores", "Martín Martinez", "Alejandro Magno", "Álvaro Álvarez",
+				"Rodrigo Rodrigez", "Roberto Figo", "Diego Sánchez", "Gabriel Ruiz", "Golzalo Gonzalez", "Victor Vector",
+				"Daniel Garzón"};
+		String[] posicionesArgentina= {"Arquero", "Defensa", "Defensa","Defensa","Defensa","Delantero","Delantero",
+				"Delantero", "Medio campo", "Medio campo","Medio campo","Delantero", "Defensa", "Medio campo", "Defensa",
+				"Medio campo","Delantero"};
+		ArrayList<String> nuevoJugadorArgentina= new ArrayList<String>();
+		ArrayList<String> posicionNuevoArgentina= new ArrayList<String>();
+		String[] estadoArgentina= {"En campo","En campo", "En campo","En campo","En campo","En campo","En campo","En campo",
+				"En campo","En campo","En campo","En banca","En banca","En banca","En banca","En banca","En banca",};
+		
+		
+		String[] inglaterra= {"Arthur Wright", "Daniel Smith", "David Cooperfield", "Oliver Twist", "Robin Robinson",
+				"Allistair Bowie", "Alan Turing", "Alfred Cooper", "Thomas Moore", "Andrew Scott",
+				"Sherlock Holmes", "Harry Potter", "James James", "Harry Styles", "Oscar Wilde", "Ethan Myers",
+				"Connor Spooner"};
+		String[] posicionesInglaterra= {"Arquero", "Defensa", "Defensa","Defensa","Defensa","Delantero","Delantero",
+				"Delantero", "Medio campo", "Medio campo","Medio campo","Delantero", "Defensa", "Medio campo", "Delantero",
+				"Defensa", "Medio campo"};
+		ArrayList<String> nuevoJugadorInglaterra= new ArrayList<String>();
+		ArrayList<String> posicionNuevoInglaterra= new ArrayList<String>();
+		String[] estadoInglaterra= {"En campo","En campo", "En campo","En campo","En campo","En campo","En campo","En campo",
+				"En campo","En campo","En campo","En banca","En banca","En banca","En banca","En banca","En banca",};
+		
+		
+		String[] brasil= {"Damiao Da Silva", "Heitor De Oliveira", "Silvio Santos", "Reginaldo Souza",
+				"Sebastiao Teixeira", "Vicente Carvalho", "Gael Almeida", "Edvaldo Rosa", "Critiano Xavier",
+				"Adriano Nevis", "Enzo Medeiros", "Flavio de Assis", "Guilherme da Castro", "Luciano Moura", "Manoel Ferreira",
+		        "Paulo De Jesus", "Rodrigo Araujo"};
+		String[] posicionesBrasil= {"Arquero", "Defensa", "Defensa","Defensa","Defensa","Delantero","Delantero",
+				"Delantero", "Medio campo", "Medio campo","Medio campo","Delantero", "Defensa", "Medio campo", "Defensa",
+				"Delantero", "Medio campo"};
+		ArrayList<String> nuevoJugadorBrasil= new ArrayList<String>();
+		ArrayList<String> posicionNuevoBrasil= new ArrayList<String>();
+		String[] estadoBrasil= {"En campo","En campo", "En campo","En campo","En campo","En campo","En campo","En campo",
+				"En campo","En campo","En campo","En banca","En banca","En banca","En banca","En banca","En banca",};
+		
+		
+		String[] italia= {"Benito Mussolini", "Michelangelo Buonarroti", "Leonardo Da Vinci", "Nerón Claudio",
+				"Luigi Rinaldi", "Francesco Bernoulli", "Alessio Bianco", "Dante Alighieri", "Julio Cesar",
+				"Galileo Galilei", "Giorgio Armani", "Adriano Rosa", "Vincenzo Marcolini", "Guiuseppe Milano", "Giovanni Giorgio",
+				"Filippo Rizzo", "Luciano Pagano"};
+		String[] posicionesItalia= {"Arquero", "Defensa", "Defensa","Defensa","Defensa","Delantero","Delantero",
+				"Delantero", "Medio campo", "Medio campo","Medio campo","Delantero", "Defensa", "Medio campo", "Delantero",
+				"Defensa", "Medio campo"};
+		ArrayList<String> nuevoJugadorItalia= new ArrayList<String>();
+		ArrayList<String> posicionNuevoItalia= new ArrayList<String>();
+		String[] estadoItalia= {"En campo","En campo", "En campo","En campo","En campo","En campo","En campo","En campo",
+				"En campo","En campo","En campo","En banca","En banca","En banca","En banca","En banca","En banca",};
+		
+		
+		String[] francia= {"Emmanuel Macron", "Victor Hugo", "Maximilien Robespierre", "Alphonse Petit",
+				"Gustave Flaubert", "Jean Jacques Rousseau", "Rene Decartes", "Luis XVI", "Jean-Paul Sartre",
+				"Julio Vernes", "Antoine de Saint-Exupéry","Maurius Lefebvre", "Philippe Lambert", "Théodore Blanc", "Joyce Durand",
+				"Orson Roux", "Thierry Fournier"};
+		String[] posicionesFrancia= {"Arquero", "Defensa", "Defensa","Defensa","Defensa","Delantero","Delantero",
+				"Delantero", "Medio campo", "Medio campo","Medio campo","Delantero", "Defensa", "Medio campo", "Defensa", "Delantero",
+				"Medio campo"};
+		ArrayList<String> nuevoJugadorFrancia= new ArrayList<String>();
+		ArrayList<String> posicionNuevoFrancia= new ArrayList<String>();
+		String[] estadoFrancia= {"En campo","En campo", "En campo","En campo","En campo","En campo","En campo","En campo",
+				"En campo","En campo","En campo","En banca","En banca","En banca","En banca","En banca","En banca",};
+		
+		
+		int menu;
+		do {
+		menu=Integer.parseInt(JOptionPane.showInputDialog("El azteca:\n"
+				+ "1. Administrar un equipo\n"
+				+ "2. Ver tabla de posiciones\n"
+				+ "3. Ver resultados de la jornada\n"
+				+ "4. Ver goleadores\n"
+				+ "5. Iniciar juego\n"
+				+ "6. Ingresar un resultado\n"
+				+ "7. Simular un campeonato\n"
+				+ "8. Salir"));
+		
+		switch (menu) {
+		
+		case 1:
+			int menu2=0;
+			
+			do {
+			menu2= Integer.parseInt(JOptionPane.showInputDialog("Seleccione la opción de su preferencia:\n"
+					+ "1. Ver jugadores y sus posiciones\n"
+					+ "2. Cambiar de posición a un jugador\n"
+					+ "3. Ingresar un nuevo jugador\n"
+					+ "4. Sustituir jugadores\n"
+					+ "5. Elegir un capitán\n"
+					+ "6. Cambiar estado\n "
+					+ "7. Regresar"));
+			switch(menu2) {
+			case 1:
+				int menu3=0;
+				do {
+				menu3= Integer.parseInt(JOptionPane.showInputDialog("Elija el equipo que desea ver:\n"
+						+" 1. Alemania\n"
+						+" 2. Argentina\n"
+						+" 3. Inglaterra\n"
+						+" 4. Brasil\n"
+						+" 5. Italia\n"
+						+" 6. Francia\n"
+						+" 7. Regresar"));
+			switch(menu3){
+			case 1:
+			for(int i=0;i<posicionesAlemania.length;i++) {
+				System.out.println(alemania[i]+ ": "+ posicionesAlemania[i]);
+			}
+			for(int j=0;j<nuevoJugadorAlemania.toArray().length;j++) {
+				System.out.println(nuevoJugadorAlemania.get(j)+": "+ posicionNuevoAlemania.get(j));
+			}
+			break;
+			case 2:	
+			for(int i=0;i<posicionesArgentina.length;i++) {
+				System.out.println(argentina[i]+ ": "+ posicionesArgentina[i]);
+			}
+			for(int j=0;j<nuevoJugadorArgentina.toArray().length;j++) {
+				System.out.println(nuevoJugadorArgentina.get(j)+": "+ posicionNuevoArgentina.get(j));
+			}
+			break;
+			case 3:
+				for(int i=0;i<posicionesInglaterra.length;i++) {
+					System.out.println(inglaterra[i]+ ": "+ posicionesInglaterra[i]);
+				}
+				for(int j=0;j<nuevoJugadorInglaterra.toArray().length;j++) {
+					System.out.println(nuevoJugadorInglaterra.get(j)+": "+ posicionNuevoInglaterra.get(j));
+				}
+				break;
+			case 4:
+				for(int i=0;i<posicionesBrasil.length;i++) {
+					System.out.println(brasil[i]+ ": "+ posicionesBrasil[i]);
+				}
+				for(int j=0;j<nuevoJugadorBrasil.toArray().length;j++) {
+					System.out.println(nuevoJugadorBrasil.get(j)+": "+ posicionNuevoBrasil.get(j));
+				}
+				break;
+			case 5:
+				for(int i=0;i<posicionesItalia.length;i++) {
+					System.out.println(italia[i]+ ": "+ posicionesItalia[i]);
+				}
+				for(int j=0;j<nuevoJugadorItalia.toArray().length;j++) {
+					System.out.println(nuevoJugadorItalia.get(j)+": "+ posicionNuevoItalia.get(j));
+				}
+				break;
+			case 6:
+				for(int i=0;i<posicionesFrancia.length;i++) {
+					System.out.println(francia[i]+ ": "+ posicionesFrancia[i]);
+				}
+				for(int j=0;j<nuevoJugadorFrancia.toArray().length;j++) {
+					System.out.println(nuevoJugadorFrancia.get(j)+": "+ posicionNuevoFrancia.get(j));
+				}
+				break;
+				}
+			}while(menu3!=7);
+				break;
+				
+			case 2:
+				int menu4=0;
+				do {
+					menu4= Integer.parseInt(JOptionPane.showInputDialog("Elija el equipo del jugador que desea cambiar de posición:\n"
+							+" 1. Alemania\n"
+							+" 2. Argentina\n"
+							+" 3. Inglaterra\n"
+							+" 4. Brasil\n"
+							+" 5. Italia\n"
+							+" 6. Francia\n"
+							+" 7. Regresar"));
+					switch(menu4) {
+					case 1:
+						String jugador= JOptionPane.showInputDialog("Ingrese el nombre del jugador tal como se muestra en la pantalla:");
+						for(int i=0;i<alemania.length;i++) {
+							if(alemania[i].equals(jugador)) {
+								System.out.println("La posición actual del jugador es: "+ posicionesAlemania[i]);
+								String nuevaPosicion= JOptionPane.showInputDialog("Ingrese la nueva posición deseada:\n"+
+								"Arquero\n"+
+								"Defensa\n"+
+								"Medio campo\n"+
+								"Delantero");
+								JOptionPane.showMessageDialog(null, "La nueva posición del jugador es: "+nuevaPosicion);
+								posicionesAlemania[i]=nuevaPosicion;
+							}
+						}
+						break;
+					case 2:
+						String jugador2= JOptionPane.showInputDialog("Ingrese el nombre del jugador tal como se muestra en la pantalla:");
+						for(int i=0;i<argentina.length;i++) {
+							if(argentina[i].equals(jugador2)) {
+								System.out.println("La posición actual del jugador es: "+ posicionesArgentina[i]);
+								String nuevaPosicion= JOptionPane.showInputDialog("Ingrese la nueva posición deseada:\n"+
+								"Arquero\n"+
+								"Defensa\n"+
+								"Medio campo\n"+
+								"Delantero");
+								JOptionPane.showMessageDialog(null, "La nueva posición del jugador es: "+nuevaPosicion);
+								posicionesArgentina[i]=nuevaPosicion;
+							}
+						}
+						break;
+					case 3:
+						String jugador3= JOptionPane.showInputDialog("Ingrese el nombre del jugador tal como se muestra en la pantalla:");
+						for(int i=0;i<inglaterra.length;i++) {
+							if(inglaterra[i].equals(jugador3)) {
+								System.out.println("La posición actual del jugador es: "+ posicionesInglaterra[i]);
+								String nuevaPosicion= JOptionPane.showInputDialog("Ingrese la nueva posición deseada:\n"+
+								"Arquero\n"+
+								"Defensa\n"+
+								"Medio campo\n"+
+								"Delantero");
+								JOptionPane.showMessageDialog(null, "La nueva posición del jugador es: "+nuevaPosicion);
+								posicionesInglaterra[i]=nuevaPosicion;
+							}
+						}
+						break;
+					case 4:
+						String jugador4= JOptionPane.showInputDialog("Ingrese el nombre del jugador tal como se muestra en la pantalla:");
+						for(int i=0;i<brasil.length;i++) {
+							if(brasil[i].equals(jugador4)) {
+								System.out.println("La posición actual del jugador es: "+ posicionesBrasil[i]);
+								String nuevaPosicion= JOptionPane.showInputDialog("Ingrese la nueva posición deseada:\n"+
+								"Arquero\n"+
+								"Defensa\n"+
+								"Medio campo\n"+
+								"Delantero");
+								JOptionPane.showMessageDialog(null, "La nueva posición del jugador es: "+nuevaPosicion);
+								posicionesBrasil[i]=nuevaPosicion;
+							}
+						}
+						break;
+					case 5:
+						String jugador5= JOptionPane.showInputDialog("Ingrese el nombre del jugador tal como se muestra en la pantalla:");
+						for(int i=0;i<italia.length;i++) {
+							if(italia[i].equals(jugador5)) {
+								System.out.println("La posición actual del jugador es: "+ posicionesItalia[i]);
+								String nuevaPosicion= JOptionPane.showInputDialog("Ingrese la nueva posición deseada:\n"+
+								"Arquero\n"+
+								"Defensa\n"+
+								"Medio campo\n"+
+								"Delantero");
+								JOptionPane.showMessageDialog(null, "La nueva posición del jugador es: "+nuevaPosicion);
+								posicionesItalia[i]=nuevaPosicion;
+							}
+						}
+						break;
+					case 6:
+						String jugador6= JOptionPane.showInputDialog("Ingrese el nombre del jugador tal como se muestra en la pantalla:");
+						for(int i=0;i<francia.length;i++) {
+							if(francia[i].equals(jugador6)) {
+								System.out.println("La posición actual del jugador es: "+ posicionesFrancia[i]);
+								String nuevaPosicion= JOptionPane.showInputDialog("Ingrese la nueva posición deseada:\n"+
+								"Arquero\n"+
+								"Defensa\n"+
+								"Medio campo\n"+
+								"Delantero");
+								JOptionPane.showMessageDialog(null, "La nueva posición del jugador es: "+nuevaPosicion);
+								posicionesFrancia[i]=nuevaPosicion;
+							}
+						}
+						break;
+						
+					}	
+				}while(menu4!=7);
+			case 3:
+				int menu5=0;
+				do {
+					menu5= Integer.parseInt(JOptionPane.showInputDialog("Elija el equipo del nuevo jugador:\n"
+							+" 1. Alemania\n"
+							+" 2. Argentina\n"
+							+" 3. Inglaterra\n"
+							+" 4. Brasil\n"
+							+" 5. Italia\n"
+							+" 6. Francia\n"
+							+" 7. Regresar"));
+					switch(menu5) {
+					case 1:
+						String nuevoJugador= JOptionPane.showInputDialog("Ingrese el nombre de su nuevo jugador: ");
+						nuevoJugadorAlemania.add(nuevoJugador);	
+						String posicionNuevo= JOptionPane.showInputDialog("Ingrese la posición del nuevo jugador: ");
+						posicionNuevoAlemania.add(posicionNuevo);
+						break;
+					case 2:	
+						String nuevoJugador2= JOptionPane.showInputDialog("Ingrese el nombre de su nuevo jugador: ");
+						nuevoJugadorArgentina.add(nuevoJugador2);	
+						String posicionNuevo2= JOptionPane.showInputDialog("Ingrese la posición del nuevo jugador: ");
+						posicionNuevoArgentina.add(posicionNuevo2);
+						break;
+					case 3:
+						String nuevoJugador3= JOptionPane.showInputDialog("Ingrese el nombre de su nuevo jugador: ");
+						nuevoJugadorInglaterra.add(nuevoJugador3);	
+						String posicionNuevo3= JOptionPane.showInputDialog("Ingrese la posición del nuevo jugador: ");
+						posicionNuevoInglaterra.add(posicionNuevo3);
+						break;
+					case 4:
+						String nuevoJugador4= JOptionPane.showInputDialog("Ingrese el nombre de su nuevo jugador: ");
+						nuevoJugadorBrasil.add(nuevoJugador4);	
+						String posicionNuevo4= JOptionPane.showInputDialog("Ingrese la posición del nuevo jugador: ");
+						posicionNuevoBrasil.add(posicionNuevo4);
+						break;
+					case 5:
+						String nuevoJugador5= JOptionPane.showInputDialog("Ingrese el nombre de su nuevo jugador: ");
+						nuevoJugadorItalia.add(nuevoJugador5);	
+						String posicionNuevo5= JOptionPane.showInputDialog("Ingrese la posición del nuevo jugador: ");
+						posicionNuevoItalia.add(posicionNuevo5);
+						break;
+					case 6:
+						String nuevoJugador6= JOptionPane.showInputDialog("Ingrese el nombre de su nuevo jugador: ");
+						nuevoJugadorFrancia.add(nuevoJugador6);	
+						String posicionNuevo6= JOptionPane.showInputDialog("Ingrese la posición del nuevo jugador: ");
+						posicionNuevoFrancia.add(posicionNuevo6);
+						
+					}
+				}while(menu5!=7);
+			case 4:
+			int menu6=0;
+			do {
+				menu6= Integer.parseInt(JOptionPane.showInputDialog("Elija el equipo del jugador que desea sustituir:\n"
+						+" 1. Alemania\n"
+						+" 2. Argentina\n"
+						+" 3. Inglaterra\n"
+						+" 4. Brasil\n"
+						+" 5. Italia\n"
+						+" 6. Francia\n"
+						+" 7. Regresar"));
+				switch(menu6) {
+				case 1:
+					for(int i=0;i<alemania.length;i++) {
+						System.out.println(alemania[i]+": "+ estadoAlemania[i]);
+					}
+					String sustituido= JOptionPane.showInputDialog("Ingrese el nombre del jugador a sustituir como\n"
+					+" se muestra en la pantalla:");
+					for(int i=0;i<alemania.length;i++) {
+						if(alemania[i].equals(sustituido)) {
+							String sustituto= JOptionPane.showInputDialog("Ingrese el nombre del jugador que ingresará\n"+
+						    "al campo");
+							estadoAlemania[i]= "En banca";
+						}
+					}
+					break;
+				case 2:
+					for(int i=0;i<argentina.length;i++) {
+						System.out.println(argentina[i]+": "+ estadoArgentina[i]);
+					}
+					String sustituido2= JOptionPane.showInputDialog("Ingrese el nombre del jugador a sustituir como\n"
+					+" se muestra en la pantalla:");
+					for(int i=0;i<argentina.length;i++) {
+						if(argentina[i].equals(sustituido2)) {
+							String sustituto2= JOptionPane.showInputDialog("Ingrese el nombre del jugador que ingresará\n"+
+						    "al campo");
+							estadoArgentina[i]= "En banca";
+						}
+					}
+					break;
+				case 3:
+					for(int i=0;i<inglaterra.length;i++) {
+						System.out.println(inglaterra[i]+": "+ estadoInglaterra[i]);
+					}
+					String sustituido3= JOptionPane.showInputDialog("Ingrese el nombre del jugador a sustituir como\n"
+					+" se muestra en la pantalla:");
+					for(int i=0;i<inglaterra.length;i++) {
+						if(inglaterra[i].equals(sustituido3)) {
+							String sustituto3= JOptionPane.showInputDialog("Ingrese el nombre del jugador que ingresará\n"+
+						    "al campo");
+							estadoInglaterra[i]= "En banca";
+						}
+					}
+					break;
+				case 4:
+					for(int i=0;i<brasil.length;i++) {
+						System.out.println(brasil[i]+": "+ estadoBrasil[i]);
+					}
+					String sustituido4= JOptionPane.showInputDialog("Ingrese el nombre del jugador a sustituir como\n"
+					+" se muestra en la pantalla:");
+					for(int i=0;i<brasil.length;i++) {
+						if(brasil[i].equals(sustituido4)) {
+							String sustituto4= JOptionPane.showInputDialog("Ingrese el nombre del jugador que ingresará\n"+
+						    "al campo");
+							estadoBrasil[i]= "En banca";
+							
+							
+						}
+					}
+					break;
+					
+					
+					
+					
+					
+					}
+				
+				
+			}while(menu6!=7);
+				
+			}
+		}while(menu2!=7);
+		
+		}
+		
+	}while(menu!=8);
+	}	
+	}
+	
